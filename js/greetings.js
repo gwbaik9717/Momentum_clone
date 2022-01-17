2c89a5f549b038f131062eb092aa9622be8cbe90
gwbaik9717 <gwbaik9717@icloud.com>
2022-01-17 22:24:36 +0900

    login completed
    - local storage
    - file name: app.js -> greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -1,14 +1,14 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
-
+const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add('hidden');
     const username = loginInput.value;
     paintGreetings(username);
-    
+    localStorage.setItem('username', username) 
 }
 
 function paintGreetings(username){
@@ -16,4 +16,13 @@ function paintGreetings(username){
     greeting.innerHTML = `Hello! ${username}`;
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove('hidden');
+    loginForm.addEventListener('submit', onLoginSubmit);
+}else{
+    paintGreetings(savedUsername);
+}
