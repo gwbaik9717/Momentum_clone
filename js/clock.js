c68725e11eedd16875eb0accfd866a89c60a0498
gwbaik9717 <gwbaik9717@icloud.com>
2022-01-18 16:03:47 +0900

    clock completed
    - setinterval

---
+++ js/clock.js
@@ -0,0 +1,10 @@
+const clock = document.querySelector('#clock');
+
+function getClock(){
+    const date =  new Date();
+    clock.innerText = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
+}
+
+
+getClock() //최초 로딩시 바로 실행
+setInterval(getClock, 1000);
