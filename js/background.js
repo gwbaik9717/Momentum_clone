5fa2b64be193e5137bc9c21826600a2c1130d280
gwbaik9717 <gwbaik9717@icloud.com>
2022-01-23 21:41:46 +0900

    background changed

--- js/background.js
+++ js/background.js
@@ -1,6 +1,8 @@
 const images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg'];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement('img');
-bgImage.src = `../images/${chosenImage}`;
+const containerEl = document.querySelector('.container');
 
-document.body.appendChild(bgImage);
+containerEl.style.backgroundImage = `url(../images/${chosenImage})`;
+
+//document.body.appendChild(bgImage);
