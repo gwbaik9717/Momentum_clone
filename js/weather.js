402aea7263c35108493cc47d0103a9866201a3ec
gwbaik9717 <gwbaik9717@icloud.com>
2022-01-23 21:40:42 +0900

    weather
    - 현재 온도 1의자리에서 버림

--- js/weather.js
+++ js/weather.js
@@ -4,12 +4,12 @@ const API_KEY = "587d7ad168f42acdf3e4bc3b4340a520";
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
+        const city = document.querySelector("#info__city");
+        const temp = document.querySelector("#info__temp span");
+        temp.innerHTML = Math.floor(data.main.temp);
         city.innerText = data.name;
-        weather.innerText = data.weather[0].main;
     });
 }
 
