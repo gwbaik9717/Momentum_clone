9e48bb3d3f7e484d1beecb6e130d4127af2a142e
gwbaik9717 <gwbaik9717@icloud.com>
2022-01-23 11:52:27 +0900

    greetings
    - 로그인시 사용자 이름과 함께 현재시간 표시
    - today-focus 추가
    - 시간 sec 제거

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,7 @@ const clock = document.querySelector('#clock');
 
 function getClock(){
     const date =  new Date();
-    clock.innerText = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
+    clock.innerText = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
 }
 
 
--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,14 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
+
+
+const focusAnswer = document.querySelector('#answer')
+const mainFocus = document.querySelector('.focus--main');
+const mainFocusInput = document.querySelector('.focus--main input');
+const todayFocus = document.querySelector('.focus--today');
+const todayFocusList = document.querySelector('.focus--today span');
+
 const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event){
@@ -8,12 +16,29 @@ function onLoginSubmit(event){
     loginForm.classList.add('hidden');
     const username = loginInput.value;
     paintGreetings(username);
-    localStorage.setItem('username', username) 
+    localStorage.setItem('username', username); 
+    paintClock();
+    paintMainFocus();
+}
+
+function onMainFocusSubmit(event){
+    event.preventDefault();
+    todayFocusList.innerHTML = mainFocusInput.value;
+    mainFocus.classList.add('hidden');
+    todayFocus.classList.remove('hidden');
 }
 
 function paintGreetings(username){
-    greeting.classList.remove('hidden');
+    //greeting.classList.remove('hidden');
     greeting.innerHTML = `Hello! ${username}`;
+}
+
+function paintClock(){
+    clock.classList.remove('hidden');
+}
+
+function paintMainFocus(){
+    mainFocus.classList.remove('hidden');
 }
 
 
@@ -25,4 +50,8 @@ if(savedUsername === null){
     loginForm.addEventListener('submit', onLoginSubmit);
 }else{
     paintGreetings(savedUsername);
+    paintClock();
+    paintMainFocus();
 }
+
+focusAnswer.addEventListener('submit', onMainFocusSubmit);
