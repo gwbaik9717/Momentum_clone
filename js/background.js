0bfe134c02bf313ffbd69452e79a81fba43ec030
gwbaik9717 <gwbaik9717@icloud.com>
2022-01-23 22:48:13 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,8 +1,7 @@
-const images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg'];
+const images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg'];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement('img');
 const containerEl = document.querySelector('.container');
 
 containerEl.style.backgroundImage = `url(../images/${chosenImage})`;
 
-//document.body.appendChild(bgImage);
